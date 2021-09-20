a2bf09cecbeb1ca3685335cb81c91d045b4f645b
Martin Mulholland <mmulholl@redhat.com>
2021-09-20 16:27:22 -0400

    more review comments

--- scripts/src/workflowtesting/checkprforci.py
+++ scripts/src/workflowtesting/checkprforci.py
@@ -16,12 +16,9 @@ def check_if_ci_only_is_modified(api_url):
 
     files_api_url = f'{api_url}/files'
     headers = {'Accept': 'application/vnd.github.v3+json'}
-    pattern_workflow = re.compile(r".github/workflows/.*")
-    pattern_script = re.compile(r"scripts/.*")
-    pattern_test = re.compile(r"tests/.*")
-    pattern_release = re.compile(r"release/release_info.json")
-    pattern_readme = re.compile(r"README.md")
-    pattern_docs = re.compile(r"docs/([\w-]+)\.md")
+
+    workflow_files = [r".github/workflows/.*", r"scripts/.*", r"tests/.*"]
+    skip_build_files = [r"release/release_info.json", r"README.md", r"docs/([\w-]+)\.md"]
     page_number = 1
     max_page_size,page_size = 100,100
 
@@ -36,17 +33,13 @@ def check_if_ci_only_is_modified(api_url):
         page_size = len(files)
         page_number += 1
 
-
         for f in files:
+            matched = False
             filename = f["filename"]
-            if pattern_workflow.match(filename):
+            if any([pattern.match(filename) for pattern in workflow_files]):
                 workflow_found = True
-            elif pattern_script.match(filename):
-                workflow_found = True
-            elif pattern_test.match(filename):
-                workflow_found = True
-            elif pattern_release.match(filename) or pattern_readme.match(filename) or pattern_docs.match(filename):
-                others_found=  True
+            elif any([pattern.match(filename) for pattern in skip_build_files]):
+                others_found = True
             else:
                 return False
 
