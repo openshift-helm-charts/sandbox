4469e88b4d33ea96dcb1cb49dd0ca9e528acc9d3
Allen Bai <abai@redhat.com>
2021-10-26 11:14:50 -0400

    tests: refactor tests/ to use classes
    Use python classes to increase the code reusability and extendability.
    
    Closes:https://issues.redhat.com/browse/HELM-246
    Signed-off-by: Allen Bai <abai@redhat.com>

---
+++ tests/functional/utils/secret.py
@@ -0,0 +1,39 @@
+"""Utility class for storing test specific settings."""
+
+from dataclasses import dataclass
+from functional.utils.utils import *
+
+
+@dataclass
+class Secret:
+    # common secrets between one-shot and recursive tests
+    test_repo: str = ''
+    bot_name: str = ''
+    bot_token: str = ''
+    pr_number: int = -1
+    vendor_type: str = ''
+    owners_file_content: str = ''
+    test_chart: str = ''
+    test_report: str = ''
+    chart_name: str = ''
+    chart_version: str = ''
+
+@dataclass
+class SecretOneShotTesting(Secret):
+    # one-shot testing
+    base_branch: str = ''
+    pr_branch: str = ''
+    pr_number: int = -1
+    vendor: str = ''
+
+@dataclass
+class SecretRecursiveTesting(Secret):
+    # recursive testing
+    software_name: str = ''
+    software_version: str = ''
+    pr_base_branch: str = ''
+    base_branches: list = None
+    pr_branches: list = None
+    dry_run: bool = True
+    notify_id: list = None
+    submitted_charts: list = None
