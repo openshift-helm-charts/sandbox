32dbe20082411e8b9399065fd59a15d94584df0d
Allen Bai <abai@redhat.com>
2021-12-08 10:21:04 -0500

    tests: remove git.lock before configuring
    Signed-off-by: Allen Bai <abai@redhat.com>

--- tests/functional/utils/chart_certification.py
+++ tests/functional/utils/chart_certification.py
@@ -52,10 +52,10 @@ vendor:
         email (str): git email to set
         """
         git_path = repo.git.rev_parse("--show-toplevel")
-        # make sure only one process is modifying the git config
-        if not pathlib.Path(f'{git_path}/.git/config.lock').exists():
-            repo.config_writer().set_value("user", "name", username).release()
-            repo.config_writer().set_value("user", "email", email).release()
+        if pathlib.Path(f'{git_path}/.git/config.lock').exists():
+            os.remove(f'{git_path}/.git/config.lock')
+        repo.config_writer().set_value("user", "name", username).release()
+        repo.config_writer().set_value("user", "email", email).release()
 
     def get_bot_name_and_token(self):
         bot_name = os.environ.get("BOT_NAME")
