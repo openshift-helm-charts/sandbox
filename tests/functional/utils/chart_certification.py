e7c31a1435906e30baad0a16d3069e708d2fb5d3
Allen Bai <abai@redhat.com>
2021-12-08 14:46:10 -0500

    tests: use parent commit tree
    Signed-off-by: Allen Bai <abai@redhat.com>

--- tests/functional/utils/chart_certification.py
+++ tests/functional/utils/chart_certification.py
@@ -51,11 +51,11 @@ vendor:
         username (str): git username to set
         email (str): git email to set
         """
-        git_path = repo.git.rev_parse("--show-toplevel")
+        git_path = self.repo.git.rev_parse("--show-toplevel")
         # make sure only one process is modifying the git config
         if not pathlib.Path(f'{git_path}/.git/config.lock').exists():
-            repo.config_writer().set_value("user", "name", username).release()
-            repo.config_writer().set_value("user", "email", email).release()
+            self.repo.config_writer().set_value("user", "name", username).release()
+            self.repo.config_writer().set_value("user", "email", email).release()
 
     def get_bot_name_and_token(self):
         bot_name = os.environ.get("BOT_NAME")
