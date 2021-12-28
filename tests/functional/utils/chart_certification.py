c8d6898c98b8e9160c5890cfc51d6e14280eaf6f
Sushanta Das <sushanta.das.ju@gmail.com>
2021-12-28 20:37:01 +0530

    Added test scenario: report in json format

--- tests/functional/utils/chart_certification.py
+++ tests/functional/utils/chart_certification.py
@@ -466,7 +466,7 @@ class ChartCertificationE2ETestSingle(ChartCertificationE2ETest):
             values = {'repository': self.secrets.test_repo,
                     'branch': self.secrets.base_branch}
             content = Template(tmpl).substitute(values)
-            report_path = f'{self.chart_directory}/{self.secrets.chart_version}/report.yaml'
+            report_path = f'{self.chart_directory}/{self.secrets.chart_version}/' + self.secrets.test_report.split('/')[-1]
             with open(report_path, 'w') as fd:
                 fd.write(content)
             
@@ -504,7 +504,7 @@ class ChartCertificationE2ETestSingle(ChartCertificationE2ETest):
                         pytest.fail("Failed to update report yaml with chart-uri")
 
             
-            self.temp_repo.git.add(f'{self.chart_directory}/{self.secrets.chart_version}/report.yaml')
+            self.temp_repo.git.add(report_path)
             self.temp_repo.git.commit(
                 '-m', f"Add {self.secrets.vendor} {self.secrets.chart_name} {self.secrets.chart_version} report")
             self.temp_repo.git.push(f'https://x-access-token:{self.secrets.bot_token}@github.com/{self.secrets.test_repo}',
