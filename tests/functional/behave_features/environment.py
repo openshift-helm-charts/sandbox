9b8c943370b0acd9d651123ea253179b878f80c8
Sushanta Das <sushanta.das.ju@gmail.com>
2022-07-18 12:19:52 +0530

    environment file changes related to initializing chart and report

--- tests/functional/behave_features/environment.py
+++ tests/functional/behave_features/environment.py
@@ -3,12 +3,10 @@ from common.utils.chart_certification import ChartCertificationE2ETestSingle
 
 @fixture
 def workflow_test(context):
-    context.workflow_test = ChartCertificationE2ETestSingle(test_name=context.test_name, test_chart=context.test_chart, test_report=context.test_report)
+    context.workflow_test = ChartCertificationE2ETestSingle(test_name=context.test_name)
     yield context.workflow_test
     context.workflow_test.cleanup()
 
 def before_scenario(context, scenario):
     context.test_name = scenario.name.split('@')[0][:-4].split(']')[1]
-    context.test_chart = 'tests/data/vault-0.17.0.tgz'
-    context.test_report = 'tests/data/report.yaml'
     use_fixture(workflow_test, context)
