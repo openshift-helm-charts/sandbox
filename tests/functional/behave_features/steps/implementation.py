58cfdcb1e17ddf7499482263a8eb60d86d9a05ff
Sushanta Das <sushanta.das.ju@gmail.com>
2022-08-01 13:32:05 +0530

    Checking running with tags

--- tests/functional/behave_features/steps/implementation.py
+++ tests/functional/behave_features/steps/implementation.py
@@ -221,3 +221,29 @@ def sha_value_does_not_match(context, error):
 def user_sends_pull_request_with_chart_tarball_and_report(context):
     context.workflow_test.push_chart(is_tarball=True)
     context.workflow_test.send_pull_request()
+
+######## Test Submitted Charts Step definitions ##########
+@given(u'there is a github workflow for testing existing charts')
+def theres_github_workflow_for_testing_charts(context):
+    print(">>>STEP DEF: there is a github workflow for testing existing charts")
+
+@when(u'a new Openshift or chart-verifier version is specified')
+def new_openshift_or_verifier_version_is_specified(context):
+    print(">>>STEP DEF: a new Openshift or chart-verifier version is specified")
+
+@when(u'the vendor type is specified, e.g. partner, and/or redhat')
+def vendor_type_is_specified(context):
+    print(">>>STEP DEF: the vendor type is specified, e.g. partner, and/or redhat")
+
+@when(u'workflow for testing existing charts is triggered')
+def workflow_is_triggered(context):
+    print(">>>STEP DEF: workflow for testing existing charts is triggered")
+
+@then(u'submission tests are run for existing charts')
+def submission_tests_run_for_submitted_charts(context):
+    print(">>>STEP DEF: submission tests are run for existing charts")
+    #context.chart_test.process_all_charts()
+
+@then(u'all results are reported back to the caller')
+def all_results_report_back_to_caller():
+    print(">>>STEP DEF: all results are reported back to the caller")
