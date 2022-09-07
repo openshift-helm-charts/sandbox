f3c6aac838b2234b383cc8c4d260549e43faa497
Martin Mulholland <mmulholl@redhat.com>
2022-09-07 10:02:59 -0400

    check report sha

--- scripts/src/workflowtesting/checkprforci.py
+++ scripts/src/workflowtesting/checkprforci.py
@@ -18,8 +18,7 @@ def check_if_ci_only_is_modified(api_url):
     headers = {'Accept': 'application/vnd.github.v3+json'}
 
     workflow_files = [re.compile(r".github/workflows/.*"),re.compile(r"scripts/.*"),re.compile(r"tests/.*")]
-    test_files = [re.compile(r"tests/functional/step_defs/test.*"),re.compile(r"tests/functional/features/.*.feature")]
-    smoke_test_files = [re.compile(r"tests/functional/features/smoke/.*"),re.compile(r"tests/functional/step_defs/.*smoke.*.py")]
+    test_files = [re.compile(r"tests/functional/step_defs/test.*"),re.compile(r"tests/functional/behave_features/.*.feature")]
     skip_build_files = [re.compile(r"release/release_info.json"),re.compile(r"README.md"),re.compile(r"docs/([\w-]+)\.md")]
     page_number = 1
     max_page_size,page_size = 100,100
@@ -41,9 +40,7 @@ def check_if_ci_only_is_modified(api_url):
             if any([pattern.match(filename) for pattern in workflow_files]):
                 workflow_found = True
                 if any([pattern.match(filename) for pattern in test_files]):
-                    if not any([pattern.match(filename) for pattern in smoke_test_files]):
-                        print(f"[INFO] full test file found: {filename}")
-                        full_tests_included = True
+                    full_tests_included = True
             elif any([pattern.match(filename) for pattern in skip_build_files]):
                 others_found = True
             else:
