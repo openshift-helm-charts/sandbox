9bc8cb22cc51f59f51fd8d9dc9bfd60a6b8614a6
Kartikey_star <41898282+github-actions[bot]@users.noreply.github.com>
2022-10-31 13:47:33 +0530

    check event logs
    Signed-off-by: Kartikey_star <41898282+github-actions[bot]@users.noreply.github.com>

--- scripts/src/pushowners/owners.py
+++ scripts/src/pushowners/owners.py
@@ -22,7 +22,7 @@ def getFileContent(files):
                 print(type(key),value)
                 if key=='providerDelivery' and value=='True':
                     provider_delivery="Yes"
-                elif key=='users' and len(users)!=0:
+                elif key=='users' and len(key)!=0:
                     users_included="Yes"
                 elif key=='vendor':
                     vendor_name=value['name']
