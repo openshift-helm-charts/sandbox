902ef23acbb79f676989bec76c7a2249596bf7d9
Sushanta Das <sushanta.das.ju@gmail.com>
2022-11-14 11:19:56 +0530

    fix directly in main

--- scripts/src/release/releaser.py
+++ scripts/src/release/releaser.py
@@ -201,6 +201,7 @@ def main():
     os.chdir(start_directory)
 
     print(f"make changes to stage from development")
+    os.chdir(args.stage_dir)
     make_required_changes(args.pr_dir,args.dev_dir,args.stage_dir)
     stage_repository=f"{organization}{gitutils.STAGE_REPO}"
     print(f"create stage pull request, repository: {stage_repository}, branch: {args.target_branch} ")
