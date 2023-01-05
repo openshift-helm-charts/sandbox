e378259c71416a6124fec34be9cbbdad11e45034
Martin Mulholland <mmulholl@redhat.com>
2023-01-05 13:11:08 -0500

    fix 12

--- scripts/src/pullrequest/prartifact.py
+++ scripts/src/pullrequest/prartifact.py
@@ -41,8 +41,8 @@ def get_modified_files(api_url):
             page_size = len(files)
             page_number += 1
 
-            for header in r.headers.Values():
-                print(f"[INFO] response header : {header} : {r.Headers[header]}")
+            for header,value in r.headers.items():
+                print(f"[INFO] response header : {header} : {value}")
 
             if "message" in files:
                 print(f'[ERROR] getting pr files: {files["message"]}')
