e51707130a5cecbd1ddd1294c6dedbf2d4e1d34c
Jose R. Gonzalez <jose@flutes.dev>
2023-05-08 12:57:46 -0500

    Handle success case explicitly before handling failure cases
    Signed-off-by: Jose R. Gonzalez <jose@flutes.dev>

--- scripts/src/pullrequest/prepare_pr_comment.py
+++ scripts/src/pullrequest/prepare_pr_comment.py
@@ -84,7 +84,7 @@ def prepare_generic_fail_comment():
         errors = open("./pr/errors").read()
         msg += "One or more errors were found while building and verifying your pull request:\n\n"
         msg += f"{errors}\n\n"
-    else: msg+= "An unspecified error has occured while building and verifying your pull request." 
+    else: msg = "An unspecified error has occured while building and verifying your pull request." 
     return msg
 
 def prepare_oc_install_fail_comment():
@@ -113,29 +113,32 @@ def main():
     chart_name = open("./pr/chart").read().strip()
     msg = get_comment_header(issue_number)
     oc_install_result = os.environ.get("OC_INSTALL_RESULT", False)
+
+    # Handle success explicitly 
     if pr_content_result == "success" and run_verifier_result == "success" and verify_result == "success" and oc_install_result == "success":
-        gitutils.add_output("pr_passed","true")
         msg += prepare_success_comment()
-    elif pr_content_result == "failure":
-        msg += prepare_pr_content_failure_comment()
-        gitutils.add_output("pr_passed","false")
-    elif run_verifier_result == "failure":
-        msg += prepare_run_verifier_failure_comment()
-        gitutils.add_output("pr_passed","false")
-    elif verify_result == "failure":
-        community_manual_review = os.environ.get("COMMUNITY_MANUAL_REVIEW",False)
-        if community_manual_review:
-            msg += prepare_community_comment()
-            gitutils.add_output("pr_passed","true")
-        else:
-            msg += prepare_failure_comment()
+        gitutils.add_output("pr_passed","true")
+    else: # Handle various failure scenarios.
+        if pr_content_result == "failure":
+            msg += prepare_pr_content_failure_comment()
             gitutils.add_output("pr_passed","false")
-    elif oc_install_result == "failure":
-        msg += prepare_oc_install_fail_comment()
-        gitutils.add_output("pr_passed","false")
-    else:
-        msg += prepare_generic_fail_comment()
-        gitutils.add_output("pr_passed","false")
+        elif run_verifier_result == "failure":
+            msg += prepare_run_verifier_failure_comment()
+            gitutils.add_output("pr_passed","false")
+        elif verify_result == "failure":
+            community_manual_review = os.environ.get("COMMUNITY_MANUAL_REVIEW",False)
+            if community_manual_review:
+                msg += prepare_community_comment()
+                gitutils.add_output("pr_passed","true")
+            else:
+                msg += prepare_failure_comment()
+                gitutils.add_output("pr_passed","false")
+        elif oc_install_result == "failure":
+            msg += prepare_oc_install_fail_comment()
+            gitutils.add_output("pr_passed","false")
+        else:
+            msg += prepare_generic_fail_comment()
+            gitutils.add_output("pr_passed","false")
 
     msg += get_comment_footer(vendor_label, chart_name)
 
