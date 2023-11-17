3d4fd4e3d0c601f39a532f8a900d437d83b60c14
Jose R. Gonzalez <komish@flutes.dev>
2023-11-16 20:34:35 -0600

    Re-add function that metric execution calls
    Signed-off-by: Jose R. Gonzalez <komish@flutes.dev>

--- scripts/src/pullrequest/prepare_pr_comment.py
+++ scripts/src/pullrequest/prepare_pr_comment.py
@@ -22,6 +22,10 @@ def get_failure_comment():
         "There were one or more errors while building and verifying your pull request."
     )
 
+def get_comment_header(pr_number):
+    return (
+        f"Thank you for submitting PR #{pr_number} for Helm Chart Certification!"
+    )
 
 def get_verifier_errors_comment():
     return "[ERROR] The submitted chart has failed certification. Reason(s):"
@@ -209,7 +213,7 @@ def main():
     community_manual_review = os.environ.get("COMMUNITY_MANUAL_REVIEW", False)
     oc_install_result = os.environ.get("OC_INSTALL_RESULT")
 
-    msg = f"Thank you for submitting PR #{issue_number} for Helm Chart Certification!"
+    msg = get_comment_header(issue_number)
 
     # Assemble the detail separately to control order in which it is added to
     # the overall output.
