5354e6b317cebfc374c8c0075d17755c12650d72
Matthias Goerens <mgoerens@redhat.com>
2024-04-16 16:44:35 +0200

    Allow to be notified if delivery method is not set
    Add the option to be notified, via a raised Exception, if the file
    (OWNERS or report.yaml) doesn't contain the keys corresponding to the
    chart's delivery method.
    
    Signed-off-by: Matthias Goerens <mgoerens@redhat.com>

--- scripts/src/owners/owners_file.py
+++ scripts/src/owners/owners_file.py
@@ -13,6 +13,10 @@ class OwnersFileError(Exception):
     pass
 
 
+class ConfigKeyMissing(Exception):
+    pass
+
+
 def get_owner_data(category, organization, chart):
     path = os.path.join("charts", category, organization, chart, "OWNERS")
     success = False
@@ -61,7 +65,28 @@ def get_chart(owner_data):
     return chart
 
 
-def get_web_catalog_only(owner_data):
+def get_web_catalog_only(owner_data, raise_if_missing=False):
+    """Check the delivery method set in the OWNERS file data
+
+    Args:
+        owner_data (dict): Content of the OWNERS file. Typically this is the return value of the
+            get_owner_data or get_owner_data_from_file function.
+        raise_if_missing (bool, optional): Wether to raise an Exception if the delivery method is
+            not set in the OWNERS data. If set to False, the function returns False.
+
+    Raises:
+        ConfigKeyMissing: if the key is not found in OWNERS and raise_if_missing is set to True
+
+    """
+    if (
+        "web_catalog_only" not in owner_data
+        and "providerDelivery" not in owner_data
+        and raise_if_missing
+    ):
+        raise ConfigKeyMissing(
+            "Neither web_catalog_only nor providerDelivery keys were set"
+        )
+
     return owner_data.get("web_catalog_only", False) or owner_data.get(
         "providerDelivery", False
     )
