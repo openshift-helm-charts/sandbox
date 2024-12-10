fd170dbfcf4d1067e4165edd958d22729894bd95
Matthias Goerens <mgoerens@redhat.com>
2024-12-10 15:57:12 +0100

    Refactor get-verifiy-param to use Submission
    Signed-off-by: Matthias Goerens <mgoerens@redhat.com>

--- scripts/src/signedchart/signedchart.py
+++ scripts/src/signedchart/signedchart.py
@@ -28,6 +28,7 @@ def check_and_prepare_signed_chart(api_url, report_path, owner_path, key_file_pa
     return signed_chart, key_in_owners, keys_match
 
 
+# TODO: MOVE ? Refacator ?
 def get_verifier_flags(tar_file, owners_file, temp_dir):
     prov_file = f"{tar_file}.prov"
     if os.path.exists(prov_file):
